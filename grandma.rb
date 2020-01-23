b92e9a1e680ef04a370fa0220b681bb4e603e49b
Jessie Tivey <jesstivey@gmail.com>
2020-01-23 23:20:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if all uppercase
+  if phrase.uppercase
      "NO, NOT SINCE 1938!"
   elsif phrase = "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
